5228d99676183a17d471e5f189f523448d309ec5
Anmol Gangwar <anmolgangwar64@gmail.com>
2025-01-02 21:47:11 +0530

    chore(image_module): removing image controller from server

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,5 @@
 import { BodyPartController, EquipmentController, MuscleController, ExerciseController } from './modules'
 import { App } from './app'
-import { ImagesController } from '#modules/images/controllers/image.controller.js'
 import { UserController } from '#modules/users/controllers/user.controller.js'
 
 const app = new App([
@@ -8,7 +7,6 @@ const app = new App([
   new MuscleController(),
   new EquipmentController(),
   new BodyPartController(),
-  new ImagesController(),
   new UserController()
 ]).getApp()
 
